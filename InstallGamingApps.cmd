2c625ebd3e57f88571e4116e91172f9b0f543bf6
EyTOdev <107267437+EyTOdev@users.noreply.github.com>
2023-02-14 13:12:33 -0200

    Add files via upload

---
+++ InstallGamingApps.cmd
@@ -0,0 +1,5 @@
+wget https://discord.com/api/downloads/distributions/app/installers/latest?channel=stable&platform=win&arch=x86
+wget https://cdn.cloudflare.steamstatic.com/client/installer/SteamSetup.exe
+wget https://launcher-public-service-prod06.ol.epicgames.com/launcher/api/installer/download/EpicGamesLauncherInstaller.msi
+wget https://www.gezginler.net/indir/i/35659/x64/
+
